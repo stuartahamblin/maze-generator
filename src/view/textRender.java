21f6789c2727158d2c8066db77c68b11871aa8ee
stuartahamblin <stuart.a.hamblin@gmail.com>
2019-02-18 22:01:39 -0600

    complete maze initializer and text render function

--- src/view/textRender.java
+++ src/view/textRender.java
@@ -2,11 +2,63 @@ package view;
 
 public class textRender {
 
-    static String[] hexRender(char celType){
-        if(celType == 1){
-            return new String[] {" __ ","/  \\", "\\__/"};
+    public static void hexPortionRender(int celType, int portion){
+        String[] fullString = {" __ ","/  \\", "\\__/"};
+        if(celType == '1'){
+            System.out.println(fullString[portion]);
         } else {
-            return new String[] {"    ","    ","    "};
+            System.out.println("    ");
         }
     }
+
+    public static String renderWall(String side, char type){
+        switch(side){
+            case "U":
+                switch(type){
+                    case '1': return " __ ";
+                    default: return "XXXX";
+                }
+            case "UL":
+                switch(type){
+                    case '1': return "/  ";
+                    default: return "   ";
+                }
+            case "DL":
+                switch(type){
+                    case '1': return "\\";
+                    default: return " ";
+                }
+            case "D":
+                switch(type){
+                    case '1': return "__";
+                    default: return "  ";
+                }
+            case "DR":
+                switch(type){
+                    case '1': return "/ ";
+                    default: return " ";
+                }
+            case "UR":
+                switch(type){
+                    case '1': return "\\";
+                    default: return " ";
+                }
+        }
+        return "THIS IS NOT WORKING";
+    }
+
+    public static void renderRows(){
+
+    }
+
+
+
+
+
+
+
+//         __
+//     __ /  \ __
+//    /  \\__//  \
+//    \__/    \__/
 }
